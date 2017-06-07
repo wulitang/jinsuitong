f0aa174ebb37dfd17945e12941e53b56c153517c
Administrator <Administrator@AGODGJ-04251359>
2017-06-07 18:25:30 +0800

    访问链接

--- js/common.js
+++ js/common.js
@@ -3,7 +3,7 @@ $('#footBox').html(html);
 function common() {
     var html = '<div class="ke-grid clearfix"><div class="foot-left"><ul>' +
         '<li> <a href="about.html">关于我们</a></li> <li> <a href="">服务介绍</a> </li>' +
-        ' <li><a href="news.html?newType=0&pageNow=1&pageSize=10">新闻动态</a> </li>' +
+        ' <li><a href="news.html">新闻动态</a> </li>' +
         '<li> <a href="center.html">招聘信息</a> </li>' +
         '<li> <a target="_blank" href="http://12366.cqsw.gov.cn/">税务服务</a> </li> </ul>' + '<div>重庆远见金税通信息系统技术有限公司<br>邮箱：123456789@qq.com<br>地址：重庆北部新区人和街道镜泊中路5号 </div></div>' +
         '<div class="foot-center"><p class="phone-namber">400-123-1234 </p><div class="work-time">周一至周五 09:00-18:00</div><ul class="foot-icon"><li><img src="img/wechat.png"></li>' +
