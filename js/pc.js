c2f807358063fb4135d21fd74c591692f4e4c459
李远 <liyuan@liyuandeMacBook-Pro.local>
2017-06-07 11:42:51 +0800

    数据对接

--- js/pc.js
+++ js/pc.js
@@ -1,11 +1,11 @@
 $(function () {
-    //获取用户信息JSON.stringify(obj)将JSON转为字符串。JSON.parse(string)将字符串转为JSON格式；
-    if($.cookie('usermessage')){
-        var data = JSON.parse($.cookie('usermessage'));
-        console.log(data);
-    }else {
-
-    }
+    // //获取用户信息JSON.stringify(obj)将JSON转为字符串。JSON.parse(string)将字符串转为JSON格式；
+    // if($.cookie('usermessage')){
+    //     var data = JSON.parse($.cookie('usermessage'));
+    //     console.log(data);
+    // }else {
+    //
+    // }
 
     //下载页关闭弹窗
     $(".success span").click(function () {
@@ -27,43 +27,24 @@ $(function () {
     $(".ems-way li").click(function () {
         $(this).addClass("active").siblings().removeClass('active');
     });
-    // var layer, laytpl;
-    // layui.use(['layer','laytpl'], function(){
-    //     layer = layui.layer;
-    //     laytpl = layui.laytpl;
-    // });
-    // $('#test1').on('click', function(){
-    //     layer.msg('Hello layer');
-    // });
-    // function getServerData(url,data,method){
-    //     var postData = {},ret;
-    //
-    //     method = method?method:'POST';
-    //     if(data){
-    //         postData = data;
-    //         postData['postUrl'] = 'http://192.168.0.100:8088'+url,
-    //             postData['method']  = method;
-    //     }else{
-    //         postData['postUrl'] = 'http://192.168.0.100:8088'+url,
-    //             postData['method']  = method;
-    //     }
-    //     $.ajax({
-    //         url:"http://192.168.0.102",
-    //         dataType: 'jsonp',
-    //         data:postData,
-    //         type: "post",
-    //         jsonp: 'callback',
-    //         async:false,    //或false,是否异步
-    //         timeout:5000,    //超时时间
-    //         success: function (retData) {
-    //             ret = retData;
-    //         },
-    //         error: function () {
-    //             //console.log('请求错误');
-    //         }
-    //     });
-    //     return ret;
-    // };
-    //
-
+});
+layui.use(['laytpl','laypage'], function(){
+    laytpl = layui.laytpl;
+    laypage= layui.laypage;
+    //使用方式跟独立组件完全一样
+    //获取用户信息JSON.stringify(obj)将JSON转为字符串。JSON.parse(string)将字符串转为JSON格式；
+    if($.cookie('usermessage')){
+        var data = JSON.parse($.cookie('usermessage'));
+        console.log(data);
+        var getTpl = welcomeData.innerHTML;
+        laytpl(getTpl).render(data, function (html) {
+            welcomeBox.innerHTML = html;
+        });
+    }else {
+        var data = [];
+        var getTpl = welcomeData.innerHTML;
+        laytpl(getTpl).render(data, function (html) {
+            welcomeBox.innerHTML = html;
+        });
+    }
 });
